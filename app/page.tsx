de835af9a559eb3fc7558ead23327f2350e8c667
Girijesh Thodupunuri <girijesh.thodupunuri@gmail.com>
2024-08-25 16:59:31 -0700

    create footer comp

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Approach from "@/components/Approach";
 import Clients from "@/components/Clients";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
+import Footer from "@/components/Footer";
 import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNav";
 import WorkExp from "@/components/WorkExp";
@@ -13,15 +14,14 @@ export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <FloatingNav
-          navItems={navItems}
-        />
+        <FloatingNav navItems={navItems} />
         <Hero />
-        <Grid/>
-        <RecentProjects/>
-        <Clients/>
-        <WorkExp/>
-        <Approach/>
+        <Grid />
+        <RecentProjects />
+        <Clients />
+        <WorkExp />
+        <Approach />
+        <Footer />
       </div>
     </main>
   );
