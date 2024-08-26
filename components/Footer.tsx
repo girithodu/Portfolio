9cb988485d2bb95b27d4950841e268917e40541e
Girijesh Thodupunuri <girijesh.thodupunuri@gmail.com>
2024-08-25 17:20:04 -0700

    remove grid

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,13 +6,6 @@ import { socialMedia } from "@/data";
 const Footer = () => {
   return (
     <footer className="w-full pt-20 pb-10" id="contact">
-      <div className="w-full absolute left-0 -bottom-72 min-h-96">
-        <img
-          src="/footer-grid.svg"
-          alt="grid"
-          className="w-full h-full opacity-50"
-        />
-      </div>
       <div className="flex flex-col items-center">
         <h1 className="heading lg:max-w-[45vw]">
           Ready to take <span className="text-purple">your</span> digital
@@ -36,8 +29,11 @@ const Footer = () => {
         </p>
         <div className="flex items-center md:gap-3 gap-6 ">
           {socialMedia.map(({ id, img }) => (
-            <div key={id} className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300">
-              <img src={img} alt={img} width={20} height={20}  />
+            <div
+              key={id}
+              className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
+            >
+              <img src={img} alt={img} width={20} height={20} />
             </div>
           ))}
         </div>
